9c4f29fd06a290dad8159812559570868140048b
PixelGuy-2112 <92736496+PixelGuy-2112@users.noreply.github.com>
2022-01-09 20:46:44 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -3,19 +3,19 @@ del /f "C:\Users\Public\Desktop\Epic Games Launcher.lnk" > out.txt 2>&1
 net config server /srvcomment:"Windows Server 2019 By Linkstricks" > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer" /V EnableAutoTray /T REG_DWORD /D 0 /F > out.txt 2>&1
 REG ADD "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Run" /f /v Wallpaper /t REG_SZ /d D:\a\wallpaper.bat
-net user administrator Linkstricks /add >nul
-net localgroup administrators administrator /add >nul
-net user administrator /active:yes >nul
+net user PixelGuy2112 2112 /add >nul
+net localgroup PixelGuy2112s PixelGuy2112 /add >nul
+net user PixelGuy2112 /active:yes >nul
 net user installer /delete
 diskperf -Y >nul
 sc config Audiosrv start= auto >nul
 sc start audiosrv >nul
-ICACLS C:\Windows\Temp /grant administrator:F >nul
-ICACLS C:\Windows\installer /grant administrator:F >nul
+ICACLS C:\Windows\Temp /grant PixelGuy2112:F >nul
+ICACLS C:\Windows\installer /grant PixelGuy2112:F >nul
 echo Successfully Installed !, If the RDP is Dead, Please Rebuild Again!
 echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Tidak bisa mendapatkan NGROK tunnel, pastikan NGROK_AUTH_TOKEN benar di Settings> Secrets> Repository secret. Mungkin VM Anda sebelumnya masih berjalan: https://dashboard.ngrok.com/status/tunnels "
-echo Username: administrator
-echo Password: Linkstricks
+echo Username: PixelGuy2112
+echo Password: 2112
 echo Please Login to your RDP!!
 ping -n 10 127.0.0.1 >nul
